0c1d37f1152bd09557b61a6f883f50ee915650ef
Hwj1220 <782835831@qq.com>
2017-12-05 08:12:21 +0800

    修改假数据，进行部分调整

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,9 +11,9 @@ Page({
     wx.request({
       url: 'https://www.easy-mock.com/mock/5a23a9a2ff38a436c591b6fa/getArticInfo',
       success: function(res) {
-        // console.log(res.data.data);
+        console.log(res.data.data.index);
         that.setData({
-            authors: res.data.data,
+            authors: res.data.data.index,
 
         })
       }
