1926c95f7f5fbbd756388fc31ae86bfc5f8de75b
Hwj1220 <782835831@qq.com>
2017-12-08 01:19:35 +0800

    something update

--- pages/stack/stack.js
+++ pages/stack/stack.js
@@ -1,16 +1,18 @@
 
 Page({
     data: {
-      stackList:[],
+      type:[],
+      id: ""
     }, 
     onLoad: function () {
          var that = this;
           wx.request({
             url:'https://www.easy-mock.com/mock/5a23a9a2ff38a436c591b6fa/getArticInfo',
             success: function(res) {
-              console.log(res.data.data);
+              console.log(res.data.data.type);
               that.setData({
-                stackList: res.data.data,
+                type: res.data.data.type,
+                id: res.data.data.List
               })
             }
           })
