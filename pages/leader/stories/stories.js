8d6dd630882095584efcdcb484b6b5e5e76d9977
Hwj1220 <782835831@qq.com>
2017-12-10 03:32:44 +0800

    完成leader详情页

--- pages/leader/stories/stories.js
+++ pages/leader/stories/stories.js
@@ -5,14 +5,27 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    authorInfo: [],
+    article: [],
+    index: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad: function (params) {
+    var that = this;
+    wx.request({
+      url: 'https://www.easy-mock.com/mock/5a23a9a2ff38a436c591b6fa/getArticInfo',
+      success: function (res) {
+        // console.log(res.data.data.index[params.id]);
+        that.setData({
+          authorInfo: res.data.data.index[params.id].articleInfo,
+          article: res.data.data.index[params.id].articleInfo,
+          
+        })  
+      }
+    })
   },
 
   /**
